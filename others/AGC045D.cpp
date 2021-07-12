df7a929b2654521c8d3eef562e41af8857fbee86
ljfcnyali <ljfcnyali@gmail.com>
2021-07-12 14:44:55 +0800

    2021.07.12 Update

--- others/AGC045D.cpp
+++ others/AGC045D.cpp
@@ -14,10 +14,11 @@ using namespace std;
 #define int long long
 typedef long long LL;
 
-const int maxn = 60;
+const int maxn = 1e7 + 10;
+const int maxm = 5010;
 const int Mod = 1e9 + 7;
 
-int n, m, fac[maxn], inv[maxn], f[maxn][maxn], g[maxn], h[maxn];
+int n, m, fac[maxn], inv[maxn], g[maxm][maxm], h[maxm], f[maxm][maxm], a[maxn], b[maxn];
 
 inline int power(int a, int b) { int r = 1; while ( b ) { if ( b & 1 ) r = r * a % Mod; a = a * a % Mod; b >>= 1; } return r; }
 inline int C(int n, int m) 
@@ -25,6 +26,8 @@ inline int C(int n, int m)
 	if ( m < 0 ) return n == -1;
 	return n < m ? 0 : fac[n] * inv[m] % Mod * inv[n - m] % Mod; 
 } 
+inline int sgn(int x) { return x & 1 ? -1 : 1; } 
+inline int get(int x) { return inv[x] * fac[x - 1] % Mod; } 
 
 signed main()
 {
@@ -32,35 +35,42 @@ signed main()
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
-	n = maxn - 1; fac[0] = inv[0] = 1; REP(i, 1, n) fac[i] = fac[i - 1 ] * i % Mod;
+	n = maxn - 10; fac[0] = inv[0] = 1; REP(i, 1, n) fac[i] = fac[i - 1] * i % Mod;
 	inv[n] = power(fac[n], Mod - 2); for ( int i = n - 1; i >= 1; -- i ) inv[i] = inv[i + 1] * (i + 1) % Mod;
-	scanf("%lld%lld", &n, &m); 
-	f[0][0] = 1; n -= m;	
-	REP(i, 0, n) REP(j, 0, m) REP(x, i, n) REP(y, j + 1, m)
+	scanf("%lld%lld", &n, &m); n -= m;	
+
+	f[0][0] = 1;
+	REP(j, 0, m) 
 	{
-		// if ( i < n && x == i && y == j + 1 ) continue ; 
-		f[x][y] = (f[x][y] + f[i][j] * C(n - i, x - i) % Mod * C(m - j - 1, y - j - 1) % Mod * fac[x + y - i - j - 1]) % Mod;
-	}
-	printf("%lld\n", f[n][m]);
-	g[0] = 1;
-	REP(i, 0, m) REP(k, 1, m - i)
-		g[i + k] = (g[i + k] + g[i] * C(m - i - 1, k - 1) % Mod * fac[k - 1]) % Mod;
-	h[0] = 1;
-	REP(i, 1, m)
-	{
-		int w = fac[n] * C(n + i - 1, i - 1)% Mod;
-		h[i] = g[i] * w % Mod;
-		REP(j, 0, i - 1) REP(k, 0, n - 1)
+		REP(i, 0, m) h[i] = f[i][j] * fac[m - i] % Mod;
+		int sum = 0;
+		REP(i, 2, m) 
 		{
-			int ret = h[j] * fac[k] % Mod * C(k + j - 1, j - 1) % Mod;
-			ret = ret * g[i - j - 1] % Mod * fac[n - k] % Mod * C(n - k + i - j - 2, i - j - 2) % Mod;
-			ret = ret * C(i, j) % Mod;
-			ret = ret * (i - j) % Mod;
-			h[i] = (h[i] - ret) % Mod;
+			sum = (sum + h[i - 2] * get(m - i + 2)) % Mod;
+			h[i] = (h[i] + sum) % Mod;
 		}
-		cerr << h[i] << endl;
-		h[i] = h[i] * power(w, Mod - 2) % Mod;
+		REP(i, 0, m) f[i][j] = h[i] * inv[m - i] % Mod;
+		REP(i, 1, m) f[i][j + 1] = (f[i][j + 1] + f[i - 1][j]) % Mod;
 	}
-	printf("%lld\n", (h[m] + Mod) % Mod);
+	REP(j, 0, m)
+	{
+		h[0] = fac[j]; int sum = 0;
+		REP(i, 1, j) { sum = (sum + h[i - 1] * get(j - i + 1)) % Mod; h[i] = sum; }
+		REP(i, 0, j) g[i][j] = h[i] * inv[j - i] % Mod;
+	}
+	g[m + 1][m + 1] = 1;
+	int ans = g[m][m] * fac[n] % Mod * C(n + m - 1, m - 1) % Mod;
+
+	REP(i, 0, m - 1) 
+	{
+		int num = 0;
+		REP(k, 0, i)
+		{
+			int w = (C(n + m - k - 2, n - k) - C(n + i - 1 - k, i - 1)) % Mod;
+			num = (num + f[i][k] % Mod * w) % Mod;
+		}
+		ans = (ans - g[m - i - 1][m - i - 1] * num % Mod * fac[n] % Mod) % Mod;
+	}
+	printf("%lld\n", (ans + Mod) % Mod);
     return 0;
 }
