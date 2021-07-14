b5d2c4349071bff4a705c8e69caf6e2503643c36
ljfcnyali <ljfcnyali@gmail.com>
2021-07-14 19:13:50 +0800

    2021.07.14 Update

--- others/LibreOJ_2733.cpp
+++ others/LibreOJ_2733.cpp
@@ -16,7 +16,7 @@ typedef long long LL;
 const int maxn = 510;
 const int INF = 0x3f3f3f3f;
 
-int n, m, a[maxn][maxn], ret, ans;
+int n, m, a[maxn][maxn], ret, ans[maxn][maxn];
 char s[maxn]; bool vis[maxn][maxn], use[maxn][maxn];
 
 inline void DFS(int x, int y, int op)
@@ -42,12 +42,25 @@ int main()
 		scanf("%s", s + 1);
 		REP(j, 1, m) a[i][j] = (s[j] == 'Z');
 	}
-	REP(i, 1, n) REP(j, 1, m)
+	REP(i, 1, n) REP(j, 1, m) ans[i][j] = INF;
+	REP(i, 1, n) 
 	{
-		ans = INF;
-		ret = 0; mem(vis); DFS(i, j, 0); ans = min(ans, ret);
-		ret = 0; mem(vis); DFS(i, j, 1); ans = min(ans, ret);
-		printf("%d%c", ans == INF ? -1 : ans * 2, j == m ? '\n' : ' ');
+		ret = 0; mem(vis);
+		REP(j, 1, m)
+		{
+			DFS(i, j, a[i][j] ^ 1); 
+			ans[i][j] = min(ans[i][j], ret);
+		}
 	}
+	REP(i, 1, n) 
+	{
+		ret = 0; mem(vis);
+		for ( int j = m; j >= 1; -- j ) 
+		{
+			DFS(i, j, a[i][j]); 
+			ans[i][j] = min(ans[i][j], ret);
+		}
+	}
+	REP(i, 1, n) { REP(j, 1, m) printf("%d ", ans[i][j] == INF ? -1 : ans[i][j] * 2); puts(""); }
     return 0;
 }
