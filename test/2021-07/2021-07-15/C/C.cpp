a37db8531cbe4ef645e4b3f224b8f562976b8388
ljfcnyali <ljfcnyali@gmail.com>
2021-07-16 11:59:36 +0800

    2021.07.16 Update

--- test/2021-07/2021-07-15/C/C.cpp
+++ test/2021-07/2021-07-15/C/C.cpp
@@ -16,7 +16,7 @@ typedef long long LL;
 
 const int maxn = 1e5 + 10;
 
-int val[7], A[7] = {0, 1, 3, 5, 4, 2, 6}, B[7] = {0, 2, 6, 3, 4, 1, 5};
+int val[7], A[7] = {0, 1, 3, 5, 2, 4, 6}, B[7] = {0, 2, 6, 3, 4, 1, 5};
 struct node { int p[7], num[7]; } ;
 
 node operator * (node a, node b)
@@ -27,11 +27,19 @@ node operator * (node a, node b)
 	return c;
 }
 
+inline node power(node a, int b)
+{
+	node r = a; -- b;
+	while ( b ) { if ( b & 1 ) r = r * a; a = a * a; b >>= 1; }
+	return r;
+}
+
 inline node Solve(int a, int b, node R, node U)
 {
 	if ( a < b ) { swap(a, b); swap(R, U); }
-	if ( a == 2 ) return R;
-	return Solve(a % b, b, R, R * U);//  * R;
+	if ( b == 1 ) return power(R, a - 1);
+	int num = a / b;
+	return Solve(a % b, b, R, power(R, num) * U) * power(R, num);
 }
 
 signed main()
@@ -45,10 +53,8 @@ signed main()
 	node R, U; REP(i, 1, 6) R.num[i] = U.num[i] = 0;
 	R.num[3] = 1; REP(i, 1, 6) R.p[i] = A[i]; 
 	U.num[6] = 1; REP(i, 1, 6) U.p[i] = B[i];
-	cout << R.p[5] << endl;
 	node ans = Solve(a, b, R, U);
 	int ret = val[5];
-	REP(i, 1, 6) cout << ans.p[i] << " "; cout << endl;
 	REP(i, 1, 6) ret += val[i] * ans.num[i];
 	printf("%lld\n", ret);
     return 0;
