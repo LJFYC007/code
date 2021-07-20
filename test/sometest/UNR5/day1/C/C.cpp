e18dc0c1c0fbf3f5c68847c3669ef4f3f61bcd37
ljfcnyali <ljfcnyali@gmail.com>
2021-07-20 15:23:36 +0800

    2021.07.20 Update

--- test/sometest/UNR5/day1/C/C.cpp
+++ test/sometest/UNR5/day1/C/C.cpp
@@ -15,69 +15,27 @@ typedef long long LL;
 
 const int maxn = 1e5 + 10;
 
-int id, n, a[maxn], c[maxn], d[maxn], p[maxn], m, fa[maxn], dis[maxn];
-vector<int> son[maxn], Edge[maxn]; bool vis[maxn];
+int id, n, m, a[maxn], f[maxn], size[maxn], p[maxn];
+vector<int> Edge[maxn];
 
-inline void DFS1(int id)
+inline void Solve(int u)
 {
-	if ( id == n + 1 ) 
+	size[u] = 1; 
+	for ( auto v : Edge[u] ) { Solve(v); size[u] += size[v]; }
+	f[u] = a[u] - size[u] + 1;
+	sort(Edge[u].begin(), Edge[u].end(), [](const int &x, const int &y) { return f[x] < f[y]; });
+	int sum = 1;
+	for ( auto v : Edge[u] ) 
 	{
-		REP(i, 1, n) printf("%d ", p[i]); puts("");
-		exit(0);	
+		f[u] = max(f[u], f[v] - sum);
+		sum += size[v];
 	}
-	REP(i, 1, n) if ( !vis[i] ) 
-	{
-		int x, y;
-		REP(j, 1, n) 
-		{
-			if ( a[j] == i ) x = j;
-			if ( c[j] == i ) y = j;
-		}
-		vector<int> a;
-		if ( dis[x] > dis[y] ) 
-		{
-			while ( dis[x] > dis[y] ) { a.push_back(x); x = fa[x]; }
-			reverse(a.begin(), a.end());
-		}
-		else while ( dis[y] > dis[x] ) { a.push_back(y); y = fa[y]; } 
-		if ( x != y ) continue ; 
-		for ( auto it : a ) swap(c[it], c[fa[it]]);
-		vis[i] = true; 
-		DFS1(id + 1);
-		vis[i] = false; 
-		reverse(a.begin(), a.end());
-		for ( auto it : a ) swap(c[it], c[fa[it]]);
-	}
-}
-
-inline void Check()
-{
-	REP(i, 1, n) c[i] = p[i];
-	DFS1(1);
 }
 
 inline void DFS(int u)
 {
-	p[u] = ++ m; 
-	for ( auto it : Edge[u] ) { dis[it] = dis[u] + 1; DFS(it); }
-}
-
-inline void Solve(int x)
-{
-	if ( x == n + 1 ) 
-	{
-		m = 0; dis[1] = 1; DFS(1);
-		Check();
-		return ; 
-	}
-	int num = son[x].size(), b[10]; 
-	REP(i, 0, num - 1) b[i] = i;
-	do
-	{
-		Edge[x].clear();
-		REP(i, 0, num - 1) Edge[x].push_back(son[x][b[i]]);
-		Solve(x + 1);
-	} while ( next_permutation(b, b + num) );
+	p[u] = ++ m;
+	for ( auto v : Edge[u] ) DFS(v);
 }
 
 int main()
@@ -86,10 +44,11 @@ int main()
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
-	scanf("%d%d", &id, &n);
-	REP(i, 1, n) scanf("%d", &a[i]);
-	REP(i, 2, n) { int x; scanf("%d", &x); son[x].push_back(i); fa[i] = x; }
+	scanf("%d%d", &id, &n); REP(i, 1, n) scanf("%d", &a[i]);
+	REP(i, 2, n) { int x; scanf("%d", &x); Edge[x].push_back(i); }
 	Solve(1);
-	puts("-1");
+	if ( f[1] != 1 ) { puts("-1"); return 0; }
+	DFS(1);
+	REP(i, 1, n) printf("%d ", p[i]); puts("");
     return 0;
 }
