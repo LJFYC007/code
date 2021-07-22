2dd20d1dc9ee8fca41e1275d64e5a60f87380375
ljfcnyali <ljfcnyali@gmail.com>
2021-07-22 15:12:23 +0800

    2021.07.22 Update

--- others/LibreOJ_111.cpp
+++ others/LibreOJ_111.cpp
@@ -1,7 +1,7 @@
 /***************************************************************
 	File name: LibreOJ_111.cpp
 	Author: ljfcnyali
-	Create time: 2021年04月09日 星期五 08时23分34秒
+	Create time: 2021年07月22日 星期四 15时00分30秒
 ***************************************************************/
 #include<bits/stdc++.h>
 
@@ -13,9 +13,9 @@ using namespace std;
 #define pii pair<int, int>
 typedef long long LL;
 
-const int maxn = 2e6 + 10;
+const int maxn = 1e6 + 10;
 
-int n, m, sa[maxn], x[maxn], y[maxn], c[maxn];
+int x[maxn], y[maxn], n, m = 255, c[maxn], sa[maxn];
 char s[maxn];
 
 int main()
@@ -24,22 +24,26 @@ int main()
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
-	scanf("%s", s + 1); n = str(s + 1); m = 150;
-	REP(i, 1, n) { x[i] = s[i]; ++ c[x[i]]; } REP(i, 1, m) c[i] += c[i - 1];
+	scanf("%s", s + 1); n = str(s + 1); REP(i, 1, n) x[i] = s[i];
+	REP(i, 1, n) ++ c[x[i]];
+	REP(i, 1, m) c[i] += c[i - 1];
 	for ( int i = n; i >= 1; -- i ) sa[c[x[i]] --] = i;
 	for ( int k = 1; k <= n; k <<= 1 ) 
 	{
-		int tot = 0; for ( int i = n; i > n - k; -- i ) y[++ tot] = i;
+		int tot = 0;
+		REP(i, n - k + 1, n) y[++ tot] = i;
 		REP(i, 1, n) if ( sa[i] > k ) y[++ tot] = sa[i] - k;
 
-		REP(i, 0, m) c[i] = 0; REP(i, 1, n) ++ c[x[i]]; REP(i, 1, m) c[i] += c[i - 1];
+		REP(i, 1, m) c[i] = 0;
+		REP(i, 1, n) ++ c[x[i]];
+		REP(i, 1, m) c[i] += c[i - 1];
 		for ( int i = n; i >= 1; -- i ) sa[c[x[y[i]]] --] = y[i];
 
-		REP(i, 1, n) y[i] = x[i];
-		tot = x[sa[1]] = 1;
-		REP(i, 2, n) x[sa[i]] = (y[sa[i]] == y[sa[i - 1]] && y[sa[i] + k] == y[sa[i - 1] + k]) ? tot : ++ tot;
+		swap(x, y); x[sa[1]] = tot = 1;
+		REP(i, 2, n) x[sa[i]] = (y[sa[i]] == y[sa[i - 1]] && y[sa[i] + k] == y[sa[i - 1] + k]) ? tot : ++ tot;	
 		m = tot;
 	}
+
 	REP(i, 1, n) printf("%d ", sa[i]); puts("");
     return 0;
 }
